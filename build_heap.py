20a2e01e72ed2396e9dead1b1d9f67b0b38e1996
JEGORS GVOZDJEVS 221RMC148 <125265638+Jegors-Gvozdjevs@users.noreply.github.com>
2023-03-19 18:20:59 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,92 @@
-# python3
-
-
 def build_heap(data):
-    swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
 
+    swaps = []
+
+    n = len(data)
+
+    for i in range(n // 2, -1, -1):
+
+        swaps += sift_down(data, i, n)
 
     return swaps
 
+ 
+
+def sift_down(data, i, n):
+
+    left_child = 2*i + 1
+
+    right_child = 2*i + 2
+
+    min_index = i
+
+    
+    if left_child < n and data[left_child] < data[min_index]:
+
+        min_index = left_child
+
+    if right_child < n and data[right_child] < data[min_index]:
+
+        min_index = right_child
+
+    if i != min_index:
+
+        data[i], data[min_index] = data[min_index], data[i]
+
+        swaps = [(i, min_index)]
+
+        swaps += sift_down(data, min_index, n)
+
+        return swaps
+
+    else:
+
+        return []
+
+ 
 
 def main():
-    
+
+    fileorno = input()
+
     # TODO : add input and corresponding checks
-    # add another input for I or F 
+
+    # add another input for I or F
+
     # first two tests are from keyboard, third test is from a file
 
+ 
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    if "I" in fileorno or "i" in fileorno:
 
-    # checks if lenght of data is the same as the said lenght
+        n = int(input())
+
+        data = list(map(int, input().split()))
+
+    elif "F" in fileorno or "f" in fileorno:
+
+        file = input()
+
+        if "a" not in file:
+
+            with open("test/" + file, 'r')as f:
+
+                n = int(f.readline())
+
+                data = list(map(int, f.readline().split()))
+
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
+    assert len(swaps) <= 4*n
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
+        # output all swaps
 
-
-    # output all swaps
     print(len(swaps))
+
     for i, j in swaps:
+
         print(i, j)
-
-
 if __name__ == "__main__":
+
     main()
