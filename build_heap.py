46bf7f578a606a9c10ca4ba9501a58dc9fd5bda1
JEGORS GVOZDJEVS 221RMC148 <125265638+Jegors-Gvozdjevs@users.noreply.github.com>
2023-03-19 23:11:55 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,13 +1,10 @@
 def build_heap(data):
 
     swaps = []
-
     n = len(data)
 
     for i in range(n // 2, -1, -1):
-
         swaps += sift_down(data, i, n)
-
     return swaps
 
  
@@ -15,45 +12,30 @@ def build_heap(data):
 def sift_down(data, i, n):
 
     left_child = 2*i + 1
-
     right_child = 2*i + 2
-
     min_index = i
 
     
     if left_child < n and data[left_child] < data[min_index]:
-
         min_index = left_child
 
     if right_child < n and data[right_child] < data[min_index]:
-
         min_index = right_child
 
     if i != min_index:
-
         data[i], data[min_index] = data[min_index], data[i]
-
         swaps = [(i, min_index)]
 
         swaps += sift_down(data, min_index, n)
-
         return swaps
 
     else:
-
         return []
 
- 
 
 def main():
 
     fileorno = input()
-
-    # TODO : add input and corresponding checks
-
-    # add another input for I or F
-
-    # first two tests are from keyboard, third test is from a file
 
  
 
@@ -87,6 +69,7 @@ def main():
     for i, j in swaps:
 
         print(i, j)
+        
 if __name__ == "__main__":
 
     main()
